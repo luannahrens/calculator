f0d7f60014caf331468a849a883ddb98eb795067
luannahrens <lahrens@gmail.com>
2025-10-16 17:41:03 -0700

    Getting  inputs from number keys and operator keys

--- javascript.js
+++ javascript.js
@@ -38,7 +38,43 @@ function operate(operator, a, b){
 
 let a = 0;
 let b = 0;
-let operator = "";
+let operator;
+let buttons = document.querySelectorAll('button');
+let buttonNumbers = document.querySelectorAll('.btnNumber');
+//let buttonNumbers = document.getElementsByClassName('btnNumber');
+let buttonOperators = document.querySelectorAll('.btn-operator');
+
+buttonNumbers.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // Your event handling logic here
+            if (!operator){
+                if (a == 0) {
+                    a = event.target.textContent;
+                } else {
+                    a += event.target.textContent;
+                }
+                console.log("a " + a);
+            } else {
+                if (b == 0) {
+                    b = event.target.textContent;
+                } else {
+                    b += event.target.textContent;
+                }
+                console.log('b ' + b);
+            }
+            
+        });
+    });
+
+buttonOperators.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        // Your event handling logic here
+        operator = event.target.textContent;
+        console.log('operator pressed: ', operator);
+    });
+});
+
+
 console.log("Operate add: " + operate("add",2,6)); //4
 console.log("add: " + add(2,2)); // 4
 console.log("subtract: " + subtract(2,2)); //0
